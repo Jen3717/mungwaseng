a4de998da964d60d8049ed3e6b4e2f3c01843e07
Jen3717 <59596418+Jen3717@users.noreply.github.com>
2020-04-17 17:59:19 +0900

    massage

---
+++ name_age.py
@@ -0,0 +1,8 @@
+def fn_ka(name = "", age =0):
+    print("나의 이름은 {}이고, 나이는 {}살 입니다.", format(name, age))
+
+fn_ka(name = 'Sam', age = 26)
+fn_ka(age = 26, name = 'Sam')
+fn_ka(name = 'Sam')
+fn_ka(age=26)
+fn_ka('Sam', 26)
